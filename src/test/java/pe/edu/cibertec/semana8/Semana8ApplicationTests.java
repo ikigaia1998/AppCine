dc0ee2ecea2b5ba1bd8d5f489d65cd121a649786
ikigaia1998 <i202117849@cibertec.edu.pe>
2023-05-08 20:13:12 -0500

    primer commit

---
+++ src/test/java/pe/edu/cibertec/semana8/Semana8ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.semana8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
